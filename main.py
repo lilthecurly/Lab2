2e443b9f92c119018778be3be4ba2566ad271195
lilthecurly <lilthecurly@gmail.com>
2023-12-05 09:17:46 +0200

    V1.1. Date range.

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ import pandas as pd
 import csv
 from datetime import datetime, timedelta
 
+start_date = datetime(2015, 1, 1)
+end_date = datetime.now()
+
 
 def get_usd_exchange_rate(date):
     url = f'https://www.cbr-xml-daily.ru/archive/{date}/daily_json.js'
@@ -13,3 +16,8 @@ def get_usd_exchange_rate(date):
         return data["Value"]["USD"]["Value"]
     else:
         return None
+
+
+def date_range(start_date, end_date):
+    for n in range(int((end_date - start_date).days) + 1):
+        yield start_date + timedelta(n)
