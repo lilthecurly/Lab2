b533ec54297cd571c2497a45f90c720afc8a6f3d
lilthecurly <lilthecurly@gmail.com>
2023-12-05 09:43:46 +0200

    v1.2. Dataset.

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ def get_usd_exchange_rate(date):
 
     if response.status_code == 200:
         data = response.json()
-        return data["Value"]["USD"]["Value"]
+        return data["Valute"]["USD"]["Value"]
     else:
         return None
 
@@ -21,3 +21,18 @@ def get_usd_exchange_rate(date):
 def date_range(start_date, end_date):
     for n in range(int((end_date - start_date).days) + 1):
         yield start_date + timedelta(n)
+
+
+with open('dataset.csv', 'w', newline='') as csvfile:
+    csv_writer = csv.writer(csvfile)
+    csv_writer.writerow(['Date', 'USD Exchange Rate'])
+
+
+    for single_date in date_range(start_date, end_date):
+        formatted_date = single_date.strftime('%Y/%m/%d')
+        usd_rate = get_usd_exchange_rate(formatted_date)
+
+        if usd_rate is not None:
+            csv_writer.writerow([formatted_date, usd_rate])
+
+print("Data saved to dataset.csv")
